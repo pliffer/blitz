e172854b7071deeff3068d287627ed476745e60d
Weslley C X Sardinha <weslleycxs@gmail.com>
2021-07-11 18:58:23 -0300

    Agora, o comando rs está disponível, para reiniciar a instancia, como no nodemon

--- pipeline/start.js
+++ pipeline/start.js
@@ -18,7 +18,44 @@ module.exports = {
 
     },
 
+    activeSpawn: null,
+    running: false,
+
     run(folder){
+
+        // @todo Melhorar, pois acho que não é a melhor maneira
+        if(!module.exports.activeSpawn){
+
+            process.stdin.setEncoding('utf8');
+
+            process.stdin.on('data', (data) => {
+
+                if(data.trim() == 'rs'){
+
+                    module.exports.running = false;
+
+                    module.exports.activeSpawn.stdin.end();
+                    module.exports.activeSpawn.kill();
+
+                    console.log(`\n`)
+                    console.log(`@info Restarting ${folder} due user rs command -----------------`.blue)
+                    console.log(`\n`)
+
+                    process.stdin.end();
+
+                    module.exports.run(folder);
+
+                } else{
+
+                    if(!module.exports.running) return;
+
+                    module.exports.activeSpawn.stdin.write(data);
+
+                }
+
+            });
+
+        }
 
         let middle = '';
 
@@ -39,7 +76,35 @@ module.exports = {
         return fs.readJson(packagePath).then(project => {
 
             Util.inheritSpawn(['node', project.main], {
-                cwd: path.join(process.cwd(), middle)
+
+                cwd: path.join(process.cwd(), middle),
+                stdio: ['pipe', 'pipe', 'pipe'],
+                // detached: true,
+
+                callback: spawn => {
+
+                    module.exports.running = true;
+                    module.exports.activeSpawn = spawn;
+
+                    spawn.stdin.on('data', (data) => {
+
+                        process.stdout.write(`X-> ${data.toString()}`);
+
+                    });
+
+                    spawn.stdout.on('data', (data) => {
+
+                        process.stdout.write(`${data.toString()}`);
+
+                    });
+
+                    spawn.stderr.on('data', (data) => {
+
+                        process.stdout.write(`${data.toString()}`);
+
+                    });
+
+                }
             });
 
         });
