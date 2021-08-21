f671ec80eeae78992c5fa647fc63c8eccc323125
Weslley C X Sardinha <weslleycxs@gmail.com>
2021-08-21 12:29:03 -0300

    Agora, o sistema identifica qual framework estamos e tenta uma conexão, atualmente só disponível para prestashop

--- pipeline/mysql-con.js
+++ pipeline/mysql-con.js
@@ -1,3 +1,6 @@
+let path = require('path');
+let fs   = require('fs-extra');
+
 let Util = require('../util.js');
 
 module.exports = {
@@ -12,9 +15,96 @@ module.exports = {
 
     async run(){
 
-        let env = await Util.getEnv();
+        let cwd = process.cwd();
 
-        if(!env) return;
+        let framework = Util.identifyFramework(cwd);
+
+        let env = {};
+
+        switch(framework){
+            case 'prestashop':
+
+                // @todo Verificar versão do prestashop
+
+                let version = 1.74;
+
+                let baseFile = 'config/settings.inc.php';
+
+                if(version >= 1.7){
+
+                    baseFile = 'app/config/parameters.php';
+
+                }
+
+                let configFile = path.join(cwd, baseFile);
+
+                let configData = fs.readFileSync(configFile, 'utf-8');
+
+                let opts = [];
+
+                if(version >= 1.7){
+
+                    configData.split("\n").forEach(line => {
+
+                        let lineMatches = line.match(/(\'|")([a-zA-Z_]+)(\'|")\s+=>\s+(\'|")(.+?)(\',|",|'|")/);
+
+                        if(lineMatches){
+                            opts[lineMatches[2]] = lineMatches[5];
+                        }
+
+                    });
+
+                }
+
+                if(opts.database_host){
+
+                    env.MYSQL_HOST = opts.database_host;
+
+                } else{
+
+                    return console.log(`@err Não há MYSQL_HOST encontrado`);
+
+                }
+
+                if(opts.database_user){
+
+                    env.MYSQL_USER = opts.database_user;
+
+                } else{
+
+                    return console.log(`@err Não há MYSQL_USER encontrado`);
+
+                }
+
+                if(opts.database_password){
+
+                    env.MYSQL_PASS = opts.database_password;
+
+                } else{
+
+                    return console.log(`@err Não há MYSQL_PASS encontrado`);
+
+                }
+
+                if(opts.database_name){
+
+                    env.MYSQL_DB = opts.database_name;
+
+                } else{
+
+                    return console.log(`@err Não há MYSQL_DB encontrado`);
+
+                }
+
+            break;
+            default:
+
+                env = await Util.getEnv();
+
+                if(!env) return;
+
+            break;
+        }
 
         if(!env.MYSQL_PASS || !env.MYSQL_USER || !env.MYSQL_HOST || !env.MYSQL_DB){
 
