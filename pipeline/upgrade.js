d96700d14619a6d06f15f5f58b5fe1a0ef2a1aa1
Weslley C X Sardinha <weslleycxs@gmail.com>
2021-10-24 17:38:06 -0300

    Adição de blitz upgrade, para atualização por um comando

---
+++ pipeline/upgrade.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const fs   = require('fs-extra');
+
+let Util = require('../util.js');
+
+module.exports = {
+
+    setup(program){
+
+        program.option('--upgrade', 'Upgrade blitz repo');
+
+        return module.exports;
+
+    },
+
+
+    run(){
+
+        let cwd = path.resolve(__dirname, '../');
+
+        Util.run('git pull origin master', data => {
+
+            if(~data.indexOf('Already up-to-date.')){
+
+                console.log('@info Nada a atualizar');
+
+            }
+
+            if(~data.indexOf(/Updating\s+[a-z0-9]{7}\.\.[a-z0-9]{7}/g)){
+
+                console.log('@info Blitz atualizado para nova versão');
+
+            }
+
+        }, {
+            cwd: cwd
+        });
+
+    }
+
+}
