9f3eea213ee999a36ee578c5eebea064be71cf47
Weslley C X Sardinha <weslleycxs@gmail.com>
2021-10-25 09:13:39 -0300

    Teste de create wordpress

--- pipeline/create.js
+++ pipeline/create.js
@@ -30,17 +30,22 @@ module.exports = {
 
                 return Util.inheritSpawn(['wget', 'https://br.wordpress.org/latest-pt_BR.zip', '-nc']).then(() => {
 
+                    console.log('Unpacking')
+
                     return Util.inheritSpawn(['unzip', 'latest-pt_BR.zip', '-d', 'latest-pt_BR-wp']);
 
                 }).then(() => {
+                    console.log('moving')
 
                     return Util.inheritSpawn(['mv', 'latest-pt_BR-wp/wordpress', cwd]);
 
                 }).then(() => {
 
+                    console.log('blank-theme')
                     return Util.inheritSpawn(['wget', 'https://access.pliffer.com.br/public/blitz/blank-theme.zip', '-nc'])
 
                 }).then(() => {
+                    console.log('unziping')
 
                     return Util.inheritSpawn(['unzip', 'blank-theme.zip', '-d', path.join(cwd, 'wp-content/themes', cwd + '-theme']);
 
