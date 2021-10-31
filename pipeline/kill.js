fb2b468ec4e6441fdd2cd9b6c260b1315faefc15
Weslley C X Sardinha <weslleycxs@gmail.com>
2021-10-31 13:06:47 -0300

    Adição de blitz kill, para facilitar o uso do ps -ef | grep <text>

---
+++ pipeline/kill.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const fs   = require('fs-extra');
+const cp   = require('child_process');
+
+let Util = require('../util.js');
+
+require('colors');
+
+module.exports = {
+
+    processes: {},
+
+    setup(program){
+
+        program.option('--kill <string>', 'Kills a process');
+
+        return module.exports;
+
+    },
+
+    async run(arg){
+
+        let count = 0;
+        let pids  = [];
+        let killPromise = [];
+
+        Util.spawn(['ps', '-ef'], (data => {
+
+            data.split("\n").forEach(line => {
+
+                if(line.indexOf('blitz --kill') !== -1) return;
+                if(line.indexOf('blitz kill') !== -1) return;
+
+                if(line.indexOf(arg) !== -1){
+
+                    let splitted = line.split(/\s+/g);
+
+                    let pid = splitted[1];
+
+                    killPromise.push(Util.spawn(['kill', pid]).then(() => {
+
+                        count++;
+                        pids.push(pid);
+
+                    }));
+
+                }
+
+            });
+
+        })).then(() => {
+
+            return Promise.all(killPromise).then(() => {
+
+                if(count){
+
+                    console.log(`@info ${count} processos foram killados (${pids})`);
+
+                }
+
+            });
+
+        });
+
+    }
+
+}
