cb251bc26fccbe9a17472b2a24145db28393d4b3
Weslley C X Sardinha <weslleycxs@gmail.com>
2021-12-19 04:32:59 -0300

    Agora é possível instalar atualizações em um arquivo específico

--- pipeline/inspector.js
+++ pipeline/inspector.js
@@ -3,6 +3,33 @@ let fs   = require('fs-extra');
 
 let Util = require('../util.js');
 
+let documentationBug = `
+                return global.db.beginTransaction(err => { /* _blitzinspector_ */ console.log("@inspector /app/models/orders.js:3724 at 1639848873687")
+
+                    let toFreePromise = [];
+
+                    reserved.forEach(row => { /* _blitzinspector_ */ console.log("@inspector /app/models/orders.js:3728 at 1639848873687")
+/*
+                        toFreePromise.push(SkuAddresses.increment(row.item_address_id, row.quantity, row.address_id, row.sku_id, row.variant_id).then(() => { /* _blitzinspector_ */ console.log("@inspector /app/models/orders.js:3730 at 1639848873687")
+
+                            howMany++;
+
+                        }));
+*/
+                    });
+                    
+                    return Promise.all(toFreePromise).then(() => { /* _blitzinspector_ */ console.log("@inspector /app/models/orders.js:3738 at 1639848873687")
+
+                        // @todo Criar notificação de quando estamos liberando os reservados
+                        // return Orders.clearReservedItems(order.id);
+
+                    }).then(() => { /* _blitzinspector_ */ console.log("@inspector /app/models/orders.js:3743 at 1639848873687")
+`;
+
+// @todo Arrumar nas situações acima em documentationBug
+// praticamente o inspetor adicionou um comentário, quebrando o comentário, então o que
+// o inspector deve fazer ao aplicar as letras é ignorar quando estiver dentro de um comentário
+
 module.exports = {
 
     columns: 80,
@@ -12,6 +39,7 @@ module.exports = {
         if(process.stdout.columns) module.exports.columns = process.stdout.columns;
 
         program.option('--inspector <command>', 'Inspect and do things with programming projects');
+        program.option('--on <dest>', 'Destination of inspection');
 
         return module.exports;
 
@@ -34,6 +62,16 @@ module.exports = {
                 let entryPath = entry;
 
                 entry = entry.replace(cwd, '');
+
+                if(module.exports.filteredFiles.length){
+
+                    for(filter of module.exports.filteredFiles){
+
+                        if(entry != '/' + filter) return;
+
+                    }
+
+                }
 
                 let newContent = "";
 
@@ -71,6 +109,16 @@ module.exports = {
                 let entryPath = entry;
 
                 entry = entry.replace(cwd, '');
+
+                if(module.exports.filteredFiles.length){
+
+                    for(filter of module.exports.filteredFiles){
+
+                        if(entry != '/' + filter) return;
+
+                    }
+
+                }
 
                 let newContent = "";
 
@@ -118,7 +166,22 @@ module.exports = {
 
     },
 
-    run(opt){
+    filteredFiles: [],
+
+    run(opt, opts){
+
+        let argv = process.argv;
+
+        argv.shift();
+        argv.shift();
+        argv.shift();
+        argv.shift();
+
+        if(argv.length == 2){
+
+            if(argv[0] == 'on') module.exports.filteredFiles.push(argv[1]);
+
+        }
 
         if(!module.exports.opts[opt]){
 
